d333956e8079c3a78f98118eefaccf12d12add58
sahin ozturk <sahin.ozturk@yahoo.com>
2025-09-22 11:59:35 -0400

    Pizza Page fixes

--- frontend/src/pages/Pizza/pizza.jsx
+++ frontend/src/pages/Pizza/pizza.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-
+import PizzaGif from '../../assets/PizzaGiff.gif';
 const Pizza = () => {
   return (
-    <div className="pizza-page" style={{ padding: 24, maxWidth: 1100, margin: '0 auto' }}>
-      <h1>Pizza Menu</h1>
+    <div className="pizza-page" style={{ padding: 24, maxWidth: 1900, margin: '0 auto' }}>
+      
 
       <div
         style={{
           position: 'relative',
           width: '100%',
           height: 0,
-          paddingTop: '77.2727%',
+          paddingTop: '62%',
           paddingBottom: 0,
           boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)',
           marginTop: '1.6em',
@@ -20,25 +20,26 @@ const Pizza = () => {
           willChange: 'transform'
         }}
       >
-        <iframe
+        <img
           title="Pizza Menu - Canva"
           loading="lazy"
-          style={{ position: 'absolute', width: '100%', height: '100%', top: 0, left: 0, border: 'none', padding: 0, margin: 0 }}
-          src="https://www.canva.com/design/DAGyif3wWqI/9VgSYi5TmDnjSHyUT0b1aw/view?embed"
+          style={{ position: 'absolute', width: '100%', height: '100%', top: 0, left: 0, border: 'none', padding: 0, objectFit:"cover",  margin: 0 }}
+          src={PizzaGif}
+          
           allowFullScreen
           allow="fullscreen"
         />
       </div>
 
-      <p style={{ color: '#666', marginTop: 12 }}>
+      {/* <p style={{ color: '#666', marginTop: 12 }}>
         <a
-          href="https://www.canva.com/design/DAGyif3wWqI/9VgSYi5TmDnjSHyUT0b1aw/view?utm_content=DAGyif3wWqI&utm_campaign=designshare&utm_medium=embeds&utm_source=link"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
           "Delicious, handcrafted pizza with a style all its own — bold, fresh, and unmistakably unique."
         </a>{' '}
-      </p>
+      </p> */}
     </div>
   );
 };
