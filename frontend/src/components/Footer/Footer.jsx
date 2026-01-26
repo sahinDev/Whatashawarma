416e04d291670bc4a34c42dda465c8411c7bba4d
sahin ozturk <sahin.ozturk@yahoo.com>
2026-01-25 22:26:33 -0500

    Gmap iframe embeded and aligned with other footer content

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -8,8 +8,21 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-content-left">
           <img className='logo' src={assets.logo} alt="" />
-          {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p> */}
-
+          {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p> */}          
+        </div>
+        <div className="map-embed">
+          <iframe 
+           title= "Our Location"
+           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2886.2295254949054!2d-79.41874112346889!3d43.66419605171903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b3529c25afc41%3A0xbcd6c05b48967eb!2sWhat%20A%20Shawarma!5e0!3m2!1sen!2sca!4v1769396799439!5m2!1sen!2sca"
+           width="600" 
+           height="450" 
+           style={{
+            border:0
+           }} 
+           allowfullscreen="" 
+           loading="lazy" 
+           referrerpolicy="no-referrer-when-downgrade"
+           />
         </div>
         <div className="footer-content-center">
           <h2>What A Shawarma</h2>
